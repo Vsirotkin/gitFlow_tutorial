12ece287212d8ebbf5fd6e76af369ab2e71272c7
Vik Sir <vsirotkin15@gmail.com>
2024-05-19 11:25:46 +0300

    added Header style

--- gitFlow/src/routes/+page.svelte
+++ gitFlow/src/routes/+page.svelte
@@ -1,19 +1,19 @@
  
-<html lang="en">
-<head>
-  <meta charset="utf-8">
- 
-  <title>GitHub Flow Course</title>
-  <meta name="description" content="GitHub Flow Course">
-  <meta name="author" content="Johannes Kettmann">
- 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
-  <link rel="stylesheet" href="/style.css">
-</head>
- 
-<body>
-  <header class="header">
-    Header
-  </header>
-</body>
-</html>
+<main>
+    <head>
+      <meta charset="utf-8">
+     
+      <title>GitHub Flow Course</title>
+      <meta name="description" content="GitHub Flow Course">
+      <meta name="author" content="Johannes Kettmann">
+     
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+      <link rel="stylesheet" href="/style.css">
+    </head>
+     
+    <body>
+      <header class="header">
+        Header
+      </header>
+    </body>
+</main>
